b324910533be6caaf947a2c8d8a2d6f5da8c671e
Renato Gomes <silvarenato180@gmail.com>
2025-01-24 22:09:19 -0300

    Adicionando anotacoes allure. Adicionando dependencia allure report. Adicionando arquivo properties de config do allure.

--- src/test/java/BookingApiTests.java
+++ src/test/java/BookingApiTests.java
@@ -1,3 +1,4 @@
+import io.qameta.allure.*;
 import io.restassured.RestAssured;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.filter.log.RequestLoggingFilter;
@@ -5,8 +6,11 @@ import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+@DisplayName(value = "TESTS IN BOOKING API")
+@Owner( value = "Renato Gomes Silva")
 public class BookingApiTests {
     private RequestSpecification requestSpecification = RestAssured.given()
             .baseUri("https://restful-booker.herokuapp.com")
@@ -14,7 +18,11 @@ public class BookingApiTests {
 
     @Test
     @DisplayName(value = "GET BOOKING BY ID")
+    @Description(value = "Consulta livros por id específico")
+    @Tag("Essentials")
+    @Severity(SeverityLevel.NORMAL)
     public void getBookingByIdTest() {
+        Allure.parameter("bookingId", 20);
         requestSpecification.when()
                 .and().pathParam("bookingid", 20)
                 .get("/booking/{bookingid}")
@@ -24,6 +32,9 @@ public class BookingApiTests {
 
     @Test
     @DisplayName(value = "GET ALL BOOKING")
+    @Description(value = "Consulta uma lista de livros")
+    @Tag("Essentials")
+    @Severity(SeverityLevel.NORMAL)
     public void getAllBookingTest() {
         requestSpecification.when()
                 .get("/booking")
@@ -33,6 +44,9 @@ public class BookingApiTests {
 
     @Test
     @DisplayName(value = "CREATE A NEW BOOKING")
+    @Description(value = "Cria um livro")
+    @Tag("Essentials")
+    @Severity(SeverityLevel.NORMAL)
     public void createNewBookingTest() {
         requestSpecification.when()
                 .body("{\n" +
